25ec3c3aa9d9054b5e3a0e66125014d81e4c5598
Benjamin Giese <bengiese22@gmail.com>
2020-02-22 13:04:41 -0600

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from recipe import Recipe
 import requests
 
 all_recipes_base_url = 'https://www.allrecipes.com/recipes/87/everyday-cooking/vegetarian/?page='
@@ -7,50 +8,74 @@ all_recipes_total_pages = 0
 
 def scrape_all_recipes_vegetarian():
 
-	recipes = []
-	ingredients = []
+    links = []
+    recipes = []
 
-	for x in range (0, all_recipes_total_pages+1):
-		page = requests.get(all_recipes_base_url+str(x))
+    for x in range (0, all_recipes_total_pages+1):
+        page = requests.get(all_recipes_base_url+str(x))
 
-		check_status_code(page)
-	
-		soup = BeautifulSoup(page.content, 'html.parser')
+        check_status_code(page)
+    
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-		recipe_cards = soup.find_all('article', class_='fixed-recipe-card')
+        recipe_cards = soup.find_all('article', class_='fixed-recipe-card')
 
-		for article in recipe_cards:
-			recipes.append(article.div.a.get('href'))
+        for article in recipe_cards:
+            links.append(article.div.a.get('href'))
 
 
-	print(recipes)
+    #print(links)
 
-	for recipe in recipes:
-		page = requests.get(recipe)
+    links = links[0:1]
 
-		check_status_code(page)
+    for link in links:
+        # init lists
+        ingredients = []
+        directions = []
 
-		soup = BeautifulSoup(page.content, 'html.parser')
+        # load specific recipe page
+        page = requests.get(link)
+        check_status_code(page)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-		ingredient_list = soup.find_all('ul', class_='checklist')
+        # get objs of ingredients and directions
+        ingredient_list = soup.find_all('ul', class_='checklist')
+        direction_list = soup.find_all('span', class_='recipe-directions__list--item')
 
-		for ingredient in ingredient_list:
-			# print(ingredient)
-			items = ingredient.find_all('span', class_='recipe-ingred_txt')
-			# print(items)
-			for item in items:
-				item_text = item.get_text()
-				if item_text is not None and item_text != 'Add all ingredients to list' and item_text != '':
-					ingredients.append(item_text)
+        for ingredient in ingredient_list:
+            items = ingredient.find_all('span', class_='recipe-ingred_txt')
+            for item in items:
+                item_text = item.get_text()
+                if item_text is not None and item_text != 'Add all ingredients to list' and item_text != '':
+                    ingredients.append(item_text)
+        
+        for direction in direction_list:
+            direction_text = direction.get_text().rstrip()
+            directions.append(direction_text)
 
-	print(ingredients)
+        while directions.count('') > 0:
+            directions.remove('')
+
+        # print('\n----------')
+        # print(ingredients)
+        # print('\n\n')
+        # print(directions)
+
+        recipes.append(Recipe(link, ingredients, directions))
+
+    # print(recipes)
+
+    for recipe in recipes:
+        print(recipe.to_string())
+
+
 
 def check_status_code(page):
-	if page.status_code is not 200:
-		print('error, on this page: ' + page + ' with this status code: ' + page.status_code)
+    if page.status_code is not 200:
+        print('error, on this page: ' + page + ' with this status code: ' + page.status_code)
 
 def main():
-	scrape_all_recipes_vegetarian()
+    scrape_all_recipes_vegetarian()
 
 if __name__ == "__main__":
-	main()
+    main()
